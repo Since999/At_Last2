a808d82a44cfe4baeb31ba9d24f04fcb9cb83023
kimrugi <kimrugi@gmail.com>
2022-07-14 15:46:59 +0900

    Barricade 추가 설치 구현

--- GameClient/D3DBase/CStaticObjectShader.h
+++ GameClient/D3DBase/CStaticObjectShader.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Shader.h"
 
+#define MAX_ADDITIONAL_OBJECT 100
 
 class CStaticObjectShader : public CTexturedShader
 {
+	static CStaticObjectShader* singleton;
 public:
+	static CStaticObjectShader* GetInstance();
+	static CStaticObjectShader* InitInstance(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature);
+
 	CStaticObjectShader();
 	virtual ~CStaticObjectShader();
 
@@ -21,12 +26,19 @@ public:
 
 	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
 	virtual void ShadowMapRender(ID3D12GraphicsCommandList* pd3dCommandList);
+
+	virtual void AddBarricade(const BarricadePos& barricade);
 protected:
 	vector<CStaticObject*> objects;
 	unsigned int object_num;
+
+	std::vector<CMesh*> mesh;
 
 	ID3D12Resource* m_pd3dcbGameObjects = NULL;
 	CB_GAMEOBJECT_INFO* m_pcbMappedGameObjects = NULL;
 
 	CShadowShader* shadow_shader = NULL;
+
+	UINT64 cb_size = 0;
+
 };
